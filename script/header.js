6260af6f2ce29e5868271fee1e4fc9a1f0a2aa09
lim072 <bestbro072@gmail.com>
2023-09-15 21:45:40 +0900

    반응형 작업

--- script/header.js
+++ script/header.js
@@ -1,9 +1,10 @@
 const gnb = document.querySelector('.gnb');
 const sub = gnb.querySelectorAll('.sub>li');
 const subBg = document.querySelector('.subBg');
-const moGnb = document.querySelector(".moGnb>li>a");
-const moSub = moGnb.querySelectorAll('.sub>li');
-
+const moGnbNav = document.querySelector(".moGnb")
+const moGnb = document.querySelectorAll(".moGnb>li");
+const moBtnOff = document.querySelector(".moBtnOff");
+const moBtnOn = document.querySelector(".moBtnOn");
 
 gnb.addEventListener("mouseenter", ()=>{
     sub.forEach((sub)=>{
@@ -21,16 +22,46 @@ gnb.addEventListener("mouseleave", ()=>{
     subBg.style.height = "0px";
 })
 
-moGnb.addEventListener("mouseenter", ()=>{
-    moSub.forEach((moSub)=>{
-        moSub.style.height = "30px";
+
+moGnb.forEach((item)=>{
+    item.addEventListener("mouseenter", (e)=>{
+        if (e.target.querySelector('ul') === null) {
+            return;
+        }
+        let moSub = item.querySelector(".sub");
+        let moSubs = moSub.querySelectorAll("li");
+        moSubs.forEach((el)=>{
+            el.style.height = "40px";
+        })
+        
+        
     })
 })
 
-moGnb.addEventListener("mouseleave", ()=>{
-    moSub.forEach((moSub)=>{
-        moSub.style.height = "0px";
+moGnb.forEach((item)=>{
+    item.addEventListener("mouseleave", (e)=>{
+        if (e.target.querySelector('ul') === null) {
+            return;
+        }
+        let moSub = item.querySelector(".sub");
+        let moSubs = moSub.querySelectorAll("li");
+        moSubs.forEach((el)=>{
+            el.style.height = "0px";
+        })
     })
 })
 
-moGnb
+moBtnOff.addEventListener("click", ()=>{
+    if(moBtnOn.style.opacity == 0){
+        moGnbNav.style.right = "0";
+        moBtnOff.style.opacity = "0"
+        moBtnOn.style.opacity = "1";
+    }else{
+        moGnbNav.style.right = "-140px";
+        moBtnOff.style.opacity = "1"
+        moBtnOn.style.opacity = "0";
+    }
+})
+
+
+
